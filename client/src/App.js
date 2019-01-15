f232836be881c56d7f2f551ef3303e053d14edf0
annielngo3 <annielngo3@gmail.com>
2019-01-15 13:36:33 -0600

    added components and page files in src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import MainPage from "./pages/MainPage";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
