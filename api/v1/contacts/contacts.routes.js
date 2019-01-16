af87b198186847d38167d852a29667fa6bf2a873
Cody Davis <Cody.1337@gmail.com>
2019-01-16 16:11:19 -0600

    removed passport from node api server. needs implimented in react

--- api/v1/contacts/contacts.routes.js
+++ api/v1/contacts/contacts.routes.js
@@ -2,16 +2,28 @@ const controller = require('./contacts.controller');
 module.exports.initRoutes = (app) => {
 
     //create new contact
-    app.post('/api/v1/contacts', (req,res,next) => {
-        res.status(500).json({data:"not set up yet"});
+    app.post('/api/v1/contacts/:user', (req,res,next) => {
+        res.status(500).json({data:`eventually I'll put contact info into ${req.params.id}`});
     });
 
-    //read contacts
-    app.get('/api/v1/contacts', (req,res,next) => {
-        res.status(200).json({data:"get contacts"});
+    //read ALL contacts
+    app.get('/api/v1/contacts/:user', (req,res,next) => {
+        res.status(200).json({data: `get contacts belonging to.... ${req.params.id}`});
+    });
+    //read ONE contact
+    app.get('/api/v1/contacts/:user/:id' , (req, res, next) => {
+        res.status(200).json({data: `get specific contact. user it belonds to ${req.params.user}, looking for ${req.params.id}`});
     });
 
     //update contact
+    app.put('/api/v1/contacts/:user/:id', (req,res,next) =>{
+        res.status(200).json({data: `i want to update a contact for user: ${req.params.user} with id ${req.params.id}`})
+
+    });
 
     //delete contact
+    app.delete('/api/v1/contacts/:user/:id', (req, res , next) => {
+        res.status(200).json({data: `I want to delete contact with the id: ${req.params.id}`});
+
+    });
 }
