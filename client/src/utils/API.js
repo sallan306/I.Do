978a39045bcbc5656ea998cffe749ced9ca8dc6d
Cody Davis <codydavis@codys-imac.lan>
2019-01-17 16:06:23 -0600

    react api file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,31 +1,56 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getContact: () => {
+
+  //==========================================================
+  //CONTACTS CRUD
+  //==========================================================
+
+  // Gets all contacts associated with user
+  getContact: (userID, contactID) => {
+    axios.get(`/api/v1/contacts/${userID}/${contactID}`, {});
   },
-  // Gets the book with the given id
-  getContacts: (id) => {},
-  // Deletes the book with the given id
-  deleteContacts: (id) => {},
-  // Saves a book to the database
-  createContact: (bookData) => {},
-  editContact: () => {},
-  getUser: function() {},
+  // Gets the contact with the given id
+  getContacts: (userID) => {
+    axios.get(`/api/v1/contacts/${userID}`, {});
+  },
+  // Deletes the contact with the given id
+  deleteContacts: (userID, contactID) => {
+    axios.delete(`/api/v1/contacts/${userID}/${contactID}`, {});
+  },
+  // Saves a Contact to the database
+  createContact: (userID, userData) => {
+    axios.post(`/api/v1/contacts/${userID}`, {userData})
+  },
+  editContact: (userID, contactID, userData) => {
+    axios.put(`/api/v1/contacts/${userID}/${contactID}`, {userData});
+  },
+
+  //==========================================================
+  //USER CRUD
+  //==========================================================
+
   createUser: (user)=>{
-    axios.post('', {})
+    axios.post(`/api/v1/users`, {user})
   },
-  updateUser: (id, contact)=>{
-    axios.put(`/api/v1/${id}`, {contact})
+  getUser: (userID) => {
+    axios.get(`/api/v1/users/${userID}`, {});
   },
-  deleteUser: (id) => {
-    axios.post(`/api/v1/${id}`);
+  //DEV PATH ONLY
+  getUsers: () => {
+    axios.get(`/dev/users`, {})
+  },
+  updateUser: (userID, contact)=>{
+    axios.put(`/api/v1/users/${userID}`, {contact})
+  },
+  deleteUser: (userID) => {
+    axios.post(`/api/v1/users/${userID}`, {});
   },
   //sms
 
   //sentTO must be "+16032755557" to use twilio.
     //any other number isnt supported due to twilio free account.
   sendText: (sendTo, message) => {
-    axios.post('/api/v1/sms', {sendTo: sendTo, txtBody: message });
+    axios.post('/api/v1/sms', {sendTo: "+16032755557", txtBody: message });
   }
 };
