5d61dc4bdeaf9e758aea6a0e280845b30d8aaaa5
Scott Allan <sallan306@gmail.com>
2019-01-17 11:17:01 -0600

    completed branch objectives

--- api/v1/sms/twilio.js
+++ api/v1/sms/twilio.js
@@ -2,8 +2,7 @@
 const accountSid = 'AC1bad91ad90b85a0b59831570b0fdbcbc';
 const authToken = '19da2b6d8a4615a7d6568e53e1ceb931';
 const twilio = require('twilio')(accountSid, authToken);
-const seedFrom = '+13512073202';
-const seedTo = '+16032755557'
+const serverNumber = '+13512073202';
 
 function handOff(req, res, next) {
 
@@ -11,16 +10,22 @@ function handOff(req, res, next) {
     var sendTo = req.body.sendTo
     twilio.messages.create(
 
-
         {
-        body: txtBody,
-        to: sendTo,  // Text this number
-        from: seedFrom // From a valid Twilio number
+            body: txtBody,
+            to: sendTo,  // Text this number
+            from: serverNumber // From a valid Twilio number
         }
 
-
     )
-    .then((message) => console.log(message.sid));
+    .then(
+            (message) => {
+                console.log(message.sid)
+                res.status(200).json({
+                    "success": true,
+                    "msg" : message
+                })
+            }
+        );
 
 }
 module.exports = handOff;
--- api/v1/sms/twilio.routes.js
+++ api/v1/sms/twilio.routes.js
@@ -1,9 +1,9 @@
 const twilio = require("./twilio");
 module.exports.initRoutes = (app)=>{
-    app.get('/api/v1/sms', (req, res, next)=>{
+    app.post('/api/v1/sms', (req, res, next)=>{
         console.log(req.user);
 
-        twilio.messages.create(req,res,next)
+        twilio(req,res,next)
         //TODO AUTHORIZATION SEEMS TO BE WORKING. REDIRECTS NEXT
     });
 }
