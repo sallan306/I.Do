cc32f3e8d42aebdb5306d091c13354e0d0cbe324
FightingSprrow <kbauertx@gmail.com>
2019-01-24 10:39:40 -0600

    indexhtml

---
+++ client/build/precache-manifest.f20de289bd43aa84945dd18927dfce95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52a578375c7fa98a155e",
+    "url": "/static/js/main.52a57837.chunk.js"
+  },
+  {
+    "revision": "400b7169f4a720b64790",
+    "url": "/static/js/1.400b7169.chunk.js"
+  },
+  {
+    "revision": "52a578375c7fa98a155e",
+    "url": "/static/css/main.eaacdc6b.chunk.css"
+  },
+  {
+    "revision": "ad69f1e7e3c75c496a008cbfe262569d",
+    "url": "/index.html"
+  }
+];
