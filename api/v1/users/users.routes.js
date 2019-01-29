6d83052d6bab3136e13ee1d1b31accbb65f5c28b
Cody Davis <Cody.1337@gmail.com>
2019-01-29 12:40:01 -0600

    added comment spots to each contact

--- api/v1/users/users.routes.js
+++ api/v1/users/users.routes.js
@@ -7,21 +7,17 @@ module.exports.initRoutes = (app)=>{
     app.post('/api/v1/users', (req, res, next) => {
         controller.addUser(req,res,next);
     });
-
     //GET A USER
     app.get('/api/v1/users', (req,res,next) => {
         controller.findSpecificUser(req,res,next);
     });
-    
     //UPDATE A USER
     app.put('/api/v1/users', (req, res, next) => {
         res.status(200).json({data: `I want to update user: ${req.user._id}`});
     });
-
     //DELETE A USER
     app.delete('/api/v1/users', (req,res,next) => {
         console.log(req.user);
         controller.DeleteThisUser(req, res, next);
     });
-
 }
