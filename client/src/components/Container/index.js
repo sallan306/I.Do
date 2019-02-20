719043b893037343f96bdea4bed3aadefa454d40
Scott Allan <sallan306@gmail.com>
2019-02-19 23:26:26 -0600

    changed styling

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -4,15 +4,6 @@ function Container(props) {
   return <div   id="containerId"
                 className={props.className} 
                 {...props}
-                // style={{float : (props.float || "none"),
-                //         marginLeft: (props.marginLeft || "30%"),
-                //         marginRight: (props.marginRight || 0),
-                //         overflow: (props.overflow || "none"),
-                //         height: (props.height || "20vh"),
-                //         width: (props.width || "40vw"),
-                //         marginTop: (props.marginTop || "0"),
-                //         paddingLeft : "0",
-                //         paddingRight : "0"}}
                         >
     {props.children}
   </div>;
