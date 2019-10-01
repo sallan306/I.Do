5c802e3d26f0e28e05bdf15d540dd1a0107015f2
sallan306 <sallan306@gmail.com>
2019-10-01 16:59:27 -0700

    removed unncessary files, added favicon information

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,20 +4,18 @@ function Nav(props) {
   return (
     <div className="container-title">
       {/* This is where the title is styled. */}
-       
-      <div id="title" style={{background: props.secondary,
-                              color: props.font}}>
+
+      <div
+        id="title"
+        style={{ background: props.secondary, color: props.font }}
+      >
         <div className="styleBox">
           <h1 id="nav-title">i.Do</h1>
           <h2 className="titleDescription">the ultimate event planning app</h2>
         </div>
       </div>
-
-      
     </div>
-    
   );
 }
 
 export default Nav;
-
